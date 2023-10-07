fefa1cd6b905f2f505b9a893d7b476f8f4eea109
Yasser Moustaine <moustaineyasser7272@gmail.com>
2023-10-07 09:15:36 -0700

    g

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description A C program for alphabet game
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		putchar (c);
+		c++;
+	}
+	putchar ('\n');
+	return (0);
+}
