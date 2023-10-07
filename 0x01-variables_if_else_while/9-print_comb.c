785d74ec4ca3f0ee6d77fcfe62d33b5425bb0c80
Yasser Moustaine <moustaineyasser7272@gmail.com>
2023-10-07 12:57:32 -0700

    f

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description A C program print from 1 to 9
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c <= 9)
+	{
+		putchar(c + 48)
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
