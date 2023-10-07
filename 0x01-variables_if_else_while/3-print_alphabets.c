19de0b2a7a03b0efd9a125c22608a6bd0d0efa44
Yasser Moustaine <moustaineyasser7272@gmail.com>
2023-10-07 10:02:04 -0700

    p

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description A C program return o uppercase
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l = 'a';
+	char c = 'A';
+
+	while (ch <= 'Z')
+	{
+		putchar(c);
+		c++
+	}
+	putchar('\n');
+
+	return (0);
+}
