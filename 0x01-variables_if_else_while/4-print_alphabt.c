9262dc51f9869b2b48c9467ea10213823ac22982
Yasser Moustaine <moustaineyasser7272@gmail.com>
2023-10-07 10:44:30 -0700

    p

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description A C program that print all alphabet and remove q and e
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c == 'e' || c == 'q')
+			c++;
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
